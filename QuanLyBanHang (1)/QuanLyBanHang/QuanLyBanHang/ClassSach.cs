82bf7fe950ea286242ec757ba147192cc54842db
Giangbuithi <giangbt.ou@gmail.com>
2016-08-24 18:15:39 +0700

    Cap nhat lai project va file script

---
+++ QuanLyBanHang (1)/QuanLyBanHang/QuanLyBanHang/ClassSach.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuanLyBanHang
+{
+    class ClassSach
+    {
+        public string MaSach { get; set; }
+        public string TenSach { get; set; }
+        public string MaTL { get; set; }
+        public string MaVT { get; set; }
+
+        public string NXB { get; set; }
+        public decimal GiaThue { get; set; }
+
+        public ClassSach(string MaS, string TenS, string MaTheLoai, string MaViTri, string nxb, decimal giathue)
+        {
+            MaSach = MaS;
+            TenSach = TenS;
+            MaTL = MaTheLoai;
+            MaVT = MaViTri;
+            NXB = nxb;
+            GiaThue = giathue;
+
+        }
+
+
+    }
+}
